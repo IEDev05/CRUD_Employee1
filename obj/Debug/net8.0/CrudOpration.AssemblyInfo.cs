e7c7fd4d83743a5c9629ee338bed6acd5db2fbae
KirtiRaval <hp@INFOELEGANT05>
2025-03-28 16:54:29 +0530

    #

--- obj/Debug/net8.0/CrudOpration.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrudOpration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudOpration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b3ec6a2ab3de4bb2887c38a4a0bdda37c247001")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudOpration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudOpration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
